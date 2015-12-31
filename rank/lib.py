2e40948f1580af8ec1eed5d43b3e5459eb99fcdc
Tim Menzies <menzies.tim@gmail.com>
2015-12-31 05:12:17 +0000

    saving

--- rank/lib.py
+++ rank/lib.py
@@ -5,3 +5,11 @@ sys.dont_write_bytecode = True
 from base import *
 
 def identity(x): return x
+
+class DefaultDict(dict):
+    """Dictionary with a default value for unknown keys."""
+    def __init__(i, default):
+        i.default = default
+    def __getitem__(i, key):
+        if key in i: return i.get(key)
+        return i.setdefault(key, i.default())
