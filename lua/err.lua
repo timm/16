5aee0b04b75855842757fc7ca1a99c8bed786338
Tim Menzies <menzies.tim@gmail.com>
2016-01-03 16:02:43 +0000

    saving

--- lua/err.lua
+++ lua/err.lua
@@ -4,23 +4,27 @@ p    = function (x) return floor(0.5+ 100*x) end
 
 do
   local y,n = 0,0
+  local function tests(t)
+          for _,x in pairs(t) do test1(x) end end 
+  local function test1(x) 
+          local passed,err = pcall(x) 
+          if passed then y = y + 1 else
+             n = n + 1
+             print("Fails:",err) end end
+  local function assess()
+          print(fmt(":y %s :n %s :percent %s%%",
+                    y,n,p(y/(0.001+y+n)))) end
   function ok( ... ) 
-    for _,test in pairs( { ... } ) do  
-      local passed,err = pcall(test) 
-      if passed then y = y + 1 else
-        n = n + 1
-        print("Fails:",err)
-  end end end
-  function status()
-    print(fmt(":y %s :n %s :percent %s%%",
-              y,n,p(y/(0.001+y+n))))
-end end  
+    t = { ... }
+    if t['n'] == 0 then status() else assess(t) end end
+end
+
 
 function aa() assert(2==1,"ads")  assert(1==1,"aa")   end
 function bb() assert(1==1,"-----")  end
 
 ok(aa, bb)
-status() 
+ok() 
    
 function has(x,lst) 
   for _,v in pairs(lst) do
