6c3b4bc4799b54b4edf88788e69b686ae9f327dd
Tim Menzies <menzies.tim@gmail.com>
2016-02-02 03:43:00 +0000

    saving

--- lua/table101.lua
+++ lua/table101.lua
@@ -1,4 +1,5 @@
-find    = string.find
+-- everyone is prep for all
+
 add     = table.insert
 function same(x) return x end
 
@@ -27,62 +28,80 @@ txt = {
     int        = ":"
 }
 
-function ignorep(x) return find(x,txt["ignore"]) end
-function intp(x)    return find(x,txt["int"])    end 
-function floatp(x)  return find(x,txt["float"])  end
+function found(x,s) print(x,s,string.find(x,s) ~= nil)
+  return string.find(x,s) ~= nil end
+function ignorep(x) return found(x,txt["ignore"]) end
+function intp(x)    return found(x,txt["int"])    end 
+function floatp(x)  return found(x,txt["float"])  end
 function nump(x)    return intp(x) or floatp(x)  or
-                           find(x,txt["more"])   or
-                           find(x,txt["less"])   end
-function goalp(x)   return find(x,txt["less"])   or
-                           find(x,txt["more"])   or
-                           find(x,txt["klass"])  end
+                           found(x,txt["more"])   or
+                           found(x,txt["less"])   end
+function goalp(x)   return found(x,txt["less"])   or
+                           found(x,txt["more"])   or
+                           found(x,txt["klass"])  end
 
-function rows(file) 
+function o(t,s)
+  for i,x in ipairs(t) do print(s,i,"["..x.."]") end
+end
+
+function csvFile(file) 
   -- kill white space, comments, ignored columns
   -- coerce nums to nums, strings to string
   io.input(file)
-  local line=io.read()
-  local first,use,numps = true,{},{}
+  local using,preps = {},{} 
   return function()
+    local line = io.read() 
     while line ~= nil do 
-      line = line:gsub("(%s+|\n)",""):gsub(txt["comment"],"")
-      local cells = explode(line,txt["sep"])
-      line = io.read()
-      local out={}
-      if line ~= "" then
-         if first then
-           for i = 1,#cells do
-             local cell = cells[i]
-             if not ignorep(cell) then
-               print("using",i)
-               use[#use+1] = i
-               numps[#use] = nump(cell) 
-             end 
-           end 
-         end
-         for i = 1,#use do
-           local j = use[i]
-           print("!!",i,use[i],cells[j])
-           local cell = cells[j]
-           if not first and numps[j] then
-              cell = tonumber(cell)
-           end
-           out[#out+1] = cell
-         end
-         first=false
-         for k,v in ipairs(out) do
-           print("kv",k,v)
-         end
+      line = pruneLine(line) 
+      if line ~= "" then 
+        local cells = explode(line, txt["sep"]) 
+        if #using==0 then
+          print(1)
+          using,preps = header(cells)
+          for i=1,#preps do print("p",preps[i]) end
+        else
+          print(2)
+          return usable(cells,preps,using) 
+        end
       end
-      return out
+      line = io.read()
     end
     return nil
   end
+end 
+
+function header(cells)
+  local using,preps = {},{}
+  for i,cell in ipairs(cells) do
+    if not ignorep(cell) then 
+      using[#using + 1] = i
+      preps[#using] = nump(cell)  
+  end end
+  return using, preps
+end
+  
+function pruneLine(str)
+    str =  string.gsub(str, txt["whitespace"], "")
+    return string.gsub(str, txt["comment"]   , "") 
 end
 
-for row in rows("data/weather.csv") do
-  print("")
-  for k,v in ipairs(row) do
-    print("33",k,v)
+function usable(cells,preps,using)
+  local out = {}
+  for _,j in ipairs(using)  do
+    local cell = cells[j]
+    print(preps[j])
+    if preps[j] then cell = tonumber(cell) end
+    add(out,  cell)
+  end
+  print(">>",#out)
+  o(out,"out")
+  return out
+end
+  
+for row in csvFile("data/weather.csv") do 
+  print(333333)
+  print(#row, row)
+  for i = 1,#row do
+    print("33",i,row[i])
   end  
 end
