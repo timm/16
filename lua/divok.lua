745119cea65ebc73fb78f4260d531179ddfca179
Tim Menzies <menzies.tim@gmail.com>
2016-02-12 04:34:59 +0000

    saving

--- lua/divok.lua
+++ lua/divok.lua
@@ -3,7 +3,7 @@ require "cols"
 
 function _sdiv()
   local t={}
-  for _ = 1,10^3 do add(t,r()^2) end
+  for _ = 1,10^4 do add(t,r()^4) end
   local sp = Split:new{get=same}  
   for _,range in ipairs(sp:div(t)) do
      print(string.format("%2d | %.4f %.4f | %5d %.4f",
--- lua/divs.lua
+++ lua/divs.lua
@@ -5,9 +5,8 @@ Split=Object:new{enough=nil, get=last, cohen=0.2, sanity=16,
                  small=nil, id=1, trivial=1.05}
 
 function Split:div(t,    all,out)
-  sorter = function(a,b) 
-             return self.get(a) < self.get(b) end
-  t   = sort(t, sorter)
+  t = sort(t, function(a,b) 
+                  return self.get(a) < self.get(b) end)
   all = Num:new():adds(map(self.get,t))
   self.small  = self.small  or all.sd*self.cohen
   self.enough = self.enough or all.n/self.sanity
@@ -16,27 +15,30 @@ function Split:div(t,    all,out)
   return out
 end
 
-function Split:div1(t, n, all, out,     cut,lo,hi)  
+function Split:div1(t,n,all,out) 
+  local cut,lo,hi
   local first, last = self.get(t[1]), self.get(t[#t])
   local range = {id=self.id, lo=first, up=last, n=#out,
                  has=t, score = all:copy()}
-  local function split(l, r, score,    old,new)
-    if last - first >= self.small then  
-      for i,x in ipairs(t) do
-        new = self.get(x)
-        l:add(new)
-        r:sub(new)
-        if new ~= old then
-          if l.n >= self.enough then
-            if  r.n < self.enough then return nil end
-            if new - first >= self.small then
-              local maybe = l.n/n * l.sd + r.n/n * r.sd
-                if maybe*self.trivial < score then
-                  cut,score,lo,hi = i,maybe,l:copy(),r:copy()
-        end end end end
-        old = new
-  end end end
-  split(Num:new(), all, all.sd) 
+  if last - first >= self.small then 
+    local l, score = Num:new(), all.sd
+    local new, old
+    for i,x in ipairs(t) do
+      new = self.get(x)
+      l:add(new)
+      all:sub(new)
+      if new ~= old then
+        if l.n >= self.enough then
+          if  all.n < self.enough then goto rest end
+          if new - first >= self.small then
+            local maybe = l.n/n*l.sd + all.n/n*all.sd
+            if maybe*self.trivial < score then
+              cut, score = i, maybe
+              lo, hi     = l:copy(), all:copy()
+      end end end end
+      old = new
+  end end 
+  ::rest::
   if cut then -- divide the ranage
     self:div1(sub(t,1,cut), n, lo, out)
     self:div1(sub(t,cut+1), n, hi, out)
